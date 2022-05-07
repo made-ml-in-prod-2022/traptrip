e4cd8abb7a028c3cb35ca22eb66f2c5cb0997375
traptrip <aletni@gmail.com>
2022-05-08 02:33:00 +0500

    ✅ add inference pipeline tests

--- tests/test_pipelines.py
+++ tests/test_pipelines.py
@@ -1,6 +1,7 @@
 import os
-
+import pytest
 from ml_project.train_pipeline import train_pipeline
+from ml_project.inference_pipeline import inference_pipeline
 
 
 def test_train_pipeline(tmp_path, config, fake_dataset_path):
@@ -16,5 +17,30 @@ def test_train_pipeline(tmp_path, config, fake_dataset_path):
     assert os.path.exists(transformer_path), "Data transformer isn't saved!"
 
 
-def test_inference_pipeline(tmp_path, config, fake_dataset_path):
-    pass
+@pytest.fixture
+def artifacts_path(tmp_path, config, fake_dataset_path):
+    checkpoint_path = str(tmp_path.joinpath(config.general.checkpoint_path))
+    config.dataset.datapath = fake_dataset_path
+    config.general.checkpoint_path = checkpoint_path
+    train_pipeline(config)
+    return checkpoint_path
+
+
+def test_inference_pipeline(
+    tmp_path, artifacts_path, config, fake_dataset, fake_dataset_path
+):
+    prediction_path = str(tmp_path.joinpath(config.inference.prediction_path))
+    config.dataset.datapath = fake_dataset_path
+    config.general.checkpoint_path = artifacts_path
+    config.inference.prediction_path = prediction_path
+
+    inference_pipeline(config)
+
+    assert os.path.exists(prediction_path), "There is no prediction file!"
+
+    with open(prediction_path, "r") as fin:
+        lines = fin.readlines()
+
+    assert len(lines) == len(
+        fake_dataset
+    ), f"Prediction size ({len(lines)}) is not equal with dataset size ({len(fake_dataset)})"
